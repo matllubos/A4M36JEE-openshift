8447fe6261139a63f9190e0ddc8d0c5d711308ca
Karel Cemus <karel.cemus@gmail.com>
2012-11-03 16:32:02 +0100

    InterfaceServer - FlightService
     - vytvořeno rozhraní, prázdná implementace

---
+++ InterfaceServer/src/main/java/cz/cvut/fel/aos/service/flight/FlightService.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.aos.service.flight;
+
+import javax.jws.WebService;
+
+/**
+ * <p>Fasáda pro klienta. Služba zapouzdřuje přístup ke třetí vrstvě tvořené business servery.</p>
+ *
+ * @author Karel Cemus
+ */
+@WebService
+public interface FlightService {
+
+}
