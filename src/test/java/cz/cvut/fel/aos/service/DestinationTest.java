d744989f8f9c859daabdc895eb408425347565c7
Karel Cemus <karel.cemus@gmail.com>
2012-12-29 11:24:15 +0100

    Test setup
     - changed validation schema from update to validate (performance)
     - added logging to Destination test

--- src/test/java/cz/cvut/fel/aos/service/DestinationTest.java
+++ src/test/java/cz/cvut/fel/aos/service/DestinationTest.java
@@ -3,6 +3,7 @@ package cz.cvut.fel.aos.service;
 import cz.cvut.fel.aos.model.Destination;
 import cz.cvut.fel.aos.service.destination.DestinationService;
 import cz.cvut.fel.util.ArquillianTest;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
@@ -11,9 +12,10 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 /** @author Karel Cemus */
+@Slf4j
 public class DestinationTest extends ArquillianTest {
 
     @Inject
@@ -30,6 +32,7 @@ public class DestinationTest extends ArquillianTest {
             codes.add( destination.getCode() );
         }
 
+        log.trace( "Found destinations: '{}'.", codes );
         assertEquals( codes, new HashSet<String>( Arrays.asList( new String[]{ "PRG", "MAD", "LHR", "INN", "VIE" } ) ) );
     }
 }
